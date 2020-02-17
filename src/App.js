8b1bf169ca2ffd21bddf7731e8d3756ad417a03f
Apple Lab 6 <applelab6@Apples-iMac.local>
2020-02-17 15:01:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Todo  = props=>(
+  <li>
+    <input type="checkbox"></input>
+    <button>delete</button>
+    <span>{props.text}</span>  
+  </li>
+)
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      todos:[],
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map(todo=> <Todo todo={todo}/>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
